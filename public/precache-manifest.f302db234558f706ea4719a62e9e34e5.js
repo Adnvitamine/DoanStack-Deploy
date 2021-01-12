300b96cba546db51db1ea528ccac784f7d0b9099
Nguyen <doan88nguyen@gmail.com>
2021-01-12 03:24:50 +0100

    New Version with major Changes

---
+++ public/precache-manifest.f302db234558f706ea4719a62e9e34e5.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76729ea39b15b98c687f5a7b8c79ae6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b72abf24cf0bb051d8",
+    "url": "/static/css/2.c5f3287d.chunk.css"
+  },
+  {
+    "revision": "263e8aad0cf9ebbfa156",
+    "url": "/static/css/main.de7b19d8.chunk.css"
+  },
+  {
+    "revision": "93b72abf24cf0bb051d8",
+    "url": "/static/js/2.2fff6244.chunk.js"
+  },
+  {
+    "revision": "8b922f29b8f0e5cea89c1a529039a7a1",
+    "url": "/static/js/2.2fff6244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263e8aad0cf9ebbfa156",
+    "url": "/static/js/main.97987237.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3ada7a9482cb9a123ad501e45053adb3",
+    "url": "/static/media/Oxygen-Bold.3ada7a94.ttf"
+  },
+  {
+    "revision": "61d9daf063ba38f2d05f8adb7267e6fd",
+    "url": "/static/media/Oxygen-Regular.61d9daf0.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "5a51dd16b7da463b520184cecec71aa2",
+    "url": "/static/media/logo2.5a51dd16.gif"
+  }
+]);
