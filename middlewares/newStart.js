baeed823fccf35001c00e3ab13e5471035038aca
Nguyen <doan88nguyen@gmail.com>
2022-07-16 01:28:09 +0200

    Alter & resync July

--- middlewares/newStart.js
+++ middlewares/newStart.js
@@ -8,18 +8,18 @@ function newStart() {
 
     db.sequelize
   .sync({
-    force: true
-    //alter: true
+    //force: true
+    alter: true
   })
   .then(() => {
     console.log(
-      "Drop and Resync Db"
+      //"Drop and Resync Db"
       //"No Drop & Resync"
-      //"Alter & Resync"
+      "Alter & Resync"
     );
-    initial();
-    createAdmin();
-    createAdminTwo();
+    //initial();
+    //createAdmin();
+    //createAdminTwo();
     
   });
 }
